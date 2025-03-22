7be86b02d5906a714e29a6985987244b9c87e79b
Andrii Filipenko <andreyfil1337@gmail.com>
2025-03-22 16:57:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const spider = document.querySelector('img.spider');
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
 
-spider.style.top = 'calc(50% - 25px)';
-spider.style.left = 'calc(50% - 25px)';
+spider.style.top = `calc(50% - ${spiderWidth}px / 2)`;
+spider.style.left = `calc(50% - ${spiderHeight}px / 2)`;
