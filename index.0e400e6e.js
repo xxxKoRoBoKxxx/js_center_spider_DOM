a2e67b11741eb74ccb1e0c2cf680f1d414417b41
Andrii Filipenko <andreyfil1337@gmail.com>
2025-03-22 17:02:17 +0200

    make build

---
+++ index.0e400e6e.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("img.spider"),e=t.offsetWidth,c=t.offsetHeight;t.style.top="calc(50% - ".concat(e,"px / 2)"),t.style.left="calc(50% - ".concat(c,"px / 2)");
+//# sourceMappingURL=index.0e400e6e.js.map
